72976997cec2b5b904586dafe1d514ac2a1fad4c
J <jonah.ahvonen@gmail.com>
2015-05-21 15:42:06 +0300

    Add some comments

--- mysql-transformer.js
+++ mysql-transformer.js
@@ -3,10 +3,12 @@ function MysqlTransformer()
 
 }
 
+//Update query to insert query transform
 MysqlTransformer.prototype.UpdateToInsert = function (sql, outputselector, onerrorcallback)
 {
-    var transformed = 'UPDATE ';
+    var transformed = 'UPDATE '; //Returned, transformed query
 
+    //Parse needed indexes from the query
     var insertStmtIdx = 11;
     var columnListIdx = sql.indexOf('(');
     var columnListLastIdx = sql.indexOf(')');
@@ -14,10 +16,12 @@ MysqlTransformer.prototype.UpdateToInsert = function (sql, outputselector, onerr
     var valueListIdx = sql.indexOf('(', valuesStmtIdx);
     var valueListLastIdx = sql.indexOf(')', valuesStmtIdx);
 
+    //Parse needed information for transform out of the query
     var tableName = this.rt(sql.substring(insertStmtIdx, columnListIdx));
     var columnList = this.columnListStringToArray(this.rt(sql.substring(columnListIdx - 1, columnListLastIdx + 1)));
     var valueList = this.valueListStringToArray(this.rt(sql.substring(valueListIdx - 1, valueListLastIdx + 1)));
 
+    //Check that column and value counts match
     if (columnList.length !== valueList.length)
     {
         if (typeof onerrorcallback !== 'undefined')
@@ -26,6 +30,7 @@ MysqlTransformer.prototype.UpdateToInsert = function (sql, outputselector, onerr
         return;
     }
 
+    //Make the transform string
     transformed += tableName + '\nSET\n';
 
     for (var i = 0; i < columnList.length; i++)
@@ -39,23 +44,28 @@ MysqlTransformer.prototype.UpdateToInsert = function (sql, outputselector, onerr
 
     transformed += 'WHERE `someid` = :somevalue;';
 
+    //Output transformed query
     $(outputselector).val(transformed);
 };
 
+//Insert query to update query transform
 MysqlTransformer.prototype.InsertToUpdate = function (sql, outputselector, onerrorcallback)
 {
-    var transformed = 'INSERT INTO ';
-    var transformed_values = 'VALUES\n(';
+    var transformed = 'INSERT INTO '; //Returned, transformed query first part
+    var transformed_values = 'VALUES\n('; //Returned, transformed query last part
 
+    //Parse needed indexes from the query
     var updateStmtIdx = 7;
     var setStmtIdx = sql.indexOf('SET');
     var whereStmtIdx = sql.indexOf('WHERE');
 
+    //Parse needed information for transform out of the query
     var tableName = this.rt(sql.substring(updateStmtIdx, setStmtIdx));
     var setlist = this.setlistStringToArray(this.rt(sql.substring(setStmtIdx + 3, whereStmtIdx)));
 
     var splitSetlist = this.splitSetlist(setlist);
 
+    //Check that column and value counts match
     if (splitSetlist.columns.length !== splitSetlist.values.length)
     {
         if (typeof onerrorcallback !== 'undefined')
@@ -64,6 +74,7 @@ MysqlTransformer.prototype.InsertToUpdate = function (sql, outputselector, onerr
         return;
     }
 
+    //Make the transform string
     transformed += tableName + '\n(';
 
     for (var i = 0; i < setlist.length; i++)
@@ -84,34 +95,37 @@ MysqlTransformer.prototype.InsertToUpdate = function (sql, outputselector, onerr
 
     transformed += ')\n' + transformed_values + ');';
 
+    //Output transformed query
     $(outputselector).val(transformed);
 };
 
+//Helper to remove tabs and newlines from a string
 MysqlTransformer.prototype.rt = function (str)
 {
     return str.replace(/(\r\n|\n|\r|\t)/gm, '').replace(' ', '');
 };
 
-//Hack to convert a column list in a string form into a JavaScript array
+//"Hack" to convert a column list in a string form into a JavaScript array
 MysqlTransformer.prototype.columnListStringToArray = function (str)
 {
     str = str.replace("(", "[").replace(")", "]").replace(/\`/g, "\'");
-    var returnarray = eval(str);
+    var returnarray = eval(str); //Eval array looking string into an actual array :)
     return returnarray;
 };
 
-//Hack to convert a value list in a string form into a JavaScript array
+//"Hack" to convert a value list in a string form into a JavaScript array
 MysqlTransformer.prototype.valueListStringToArray = function (str)
 {
     var returnarray = str.replace("(", "").replace(")", "").replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, "").split(',');
     return returnarray;
 };
 
-//Hack to convert a set list in a string form into a JavaScript array
+//"Hack" to convert a set list in a string form into a JavaScript array
 MysqlTransformer.prototype.setlistStringToArray = function (str)
 {
     var returnarray = str.replace(/,/g, "=").replace(/\`/g, "").replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, "").split('=');
 
+    //Try to do some parsing so that numeric data is kept numeric
     for (var rar in returnarray)
     {
         var set = returnarray[rar];
@@ -121,14 +135,19 @@ MysqlTransformer.prototype.setlistStringToArray = function (str)
             set = parseInt(set);
         }
     }
+
     return returnarray;
 };
 
+//Helper function for numeric checking
 MysqlTransformer.prototype.isNumeric = function (somevar)
 {
     return !isNaN(somevar);
 };
 
+//Splits the setlist into columns and values. The setlist is first parsed into a single array where
+//every other index is column and every other index a value, so we want to separate those into their own
+//arrays
 MysqlTransformer.prototype.splitSetlist = function (setlist)
 {
     var lists = {
